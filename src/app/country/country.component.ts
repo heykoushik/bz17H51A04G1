abd82523076066552f5ad0685ca190159531b4aa
heykoushik <heykoushik@gmail.com>
2020-01-08 14:37:59 +0530

    completed

--- src/app/country/country.component.ts
+++ src/app/country/country.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
-
+import { DataService } from '../data.service';
 @Component({
   selector: 'app-country',
   templateUrl: './country.component.html',
   styleUrls: ['./country.component.css']
 })
 export class CountryComponent implements OnInit {
+ countries;
+  constructor(private data: DataService) { }
 
-  constructor() { }
 
   ngOnInit() {
+    this.data.getCountries().subscribe(d => { this.countries = d; 
+    })
   }
 
 }
